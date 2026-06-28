fad929f9c27aa0062fc886d5f29254f9e1e7d0f6
Arun <karun74315@gmail.com>
2026-06-28 21:41:55 +0530

    adding array file

---
+++ Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Array{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of  the array :");
+        int size = sc.nextInt();
+
+    int arr[] = new int[size];
+    System.out.println("enter " + size + "elements:" );
+    // arr[0]=10;
+    // arr[1]=100;
+    //  arr[2]=110;
+    //   arr[3]=130;
+    //    arr[4]=170;
+      for(int i = 0;i<arr.length;i++){
+        arr[i] = sc.nextInt();
+        System.out.println(arr[i]);
+      }
+    }
+    
+}
---
+++ Methods.java
@@ -0,0 +1,32 @@
+public class Methods{
+    // public static int Solve(){
+    //     int a = 10 ; int b = 20;
+    //     int sum = a+b;
+    //     return sum;
+    // }
+
+    // public static int print(int x, int y){
+        
+    //     int sum = x+y;
+    //     return sum;
+    // }
+    // public static void swap(int a , int b){
+    //      int temp = a;
+    //     a=b;
+    //     b=temp;
+    //     // System.out.println(swap());
+    // }
+
+
+    public static void main(String[] args){
+        // System.out.println(print(34 , 87));
+        // System.out.println(print(98, 76));
+
+        // int a = 10; int b = 30;
+        // swap(a,b);
+        // System.out.println("in main swap " + a+ ", " +  " ," + b);
+
+
+       
+    }
+}
